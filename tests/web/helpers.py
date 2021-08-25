5a553d93fbe7f8409acc2eb3b737cc9cde96d04d
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-08-25 13:38:16 +0300

    six 6

--- tests/web/helpers.py
+++ tests/web/helpers.py
@@ -3,7 +3,7 @@ from urllib.error import HTTPError, URLError  # noqa
 from urllib.parse import (  # noqa
     quote, urlencode, urljoin, urlparse,
 )
-from .urllib.request import (  # noqa
+from urllib.request import (  # noqa
     HTTPBasicAuthHandler, HTTPCookieProcessor, HTTPDigestAuthHandler, Request,
     build_opener, install_opener, urlopen,
 )
